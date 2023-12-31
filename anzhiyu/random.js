1f924c381cb5991a2a0ab1fc780a3f33bbcfa8c5
Xne3 <1154984797@qq.com>
2023-12-31 17:07:09 +0800

    Site updated: 2023-12-31 17:07:08

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/12/30/hello-world/"];function toRandomPost(){
+var posts=["2023/12/30/hello-world/","2023/12/30/你好/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
