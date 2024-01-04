a35ff1106eea67d80cbae3088988ca92994f5630
Xne3 <1154984797@qq.com>
2024-01-04 17:37:50 +0800

    Site updated: 2024-01-04 17:37:49

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/01/03/markdown/"];function toRandomPost(){
+var posts=["2024/01/03/markdown/","2024/01/03/mark/markdown - 副本/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
